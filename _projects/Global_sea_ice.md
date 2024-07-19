2b2d6ae20066e4a504f5514166559476f2e111e8
Edward Doddridge <edward.doddridge@utas.edu.au>
2024-07-19 10:16:15 +1000

    Add global sea ice graphs

---
+++ _projects/Global_sea_ice.md
@@ -0,0 +1,56 @@
+---
+layout: page
+title: Global sea ice extent
+description:
+img: assets/img/Global_SIE_anom.png
+importance: 2
+category: sea-ice
+related_publications: true
+---
+
+Global sea ice extent.
+
+<div class="row">
+    <div class="col-sm mt-3 mt-md-0">
+        {% include figure.liquid loading="eager" path="assets/img/Global_SIE_by_year.png" title="example image" class="img-fluid rounded z-depth-1" %}
+    </div>
+</div>
+<div class="caption">
+    Global sea ice extent plotted by year.
+</div>
+
+<div class="row">
+    <div class="col-sm mt-3 mt-md-0">
+        {% include figure.liquid loading="eager" path="assets/img/Global_SIE_anom.png" title="example image" class="img-fluid rounded z-depth-1" %}
+    </div>
+</div>
+<div class="caption">
+    Anomalies in Global sea ice extent.
+</div>
+
+<div class="row">
+    <div class="col-sm mt-3 mt-md-0">
+        {% include figure.liquid loading="eager" path="assets/img/Global_SIE_year_anoms.png" title="example image" class="img-fluid rounded z-depth-1" %}
+    </div>
+</div>
+<div class="caption">
+    Anomalies in Global sea ice extent plotted by year.
+</div>
+
+<div class="row">
+    <div class="col-sm mt-3 mt-md-0">
+        {% include figure.liquid loading="eager" path="assets/img/Global_SIE_year_anoms_standardised.png" title="example image" class="img-fluid rounded z-depth-1" %}
+    </div>
+</div>
+<div class="caption">
+    Anomalies in Global sea ice extent, normalised by 1981-2010 climatology, plotted by year.
+</div>
+
+<div class="row">
+    <div class="col-sm mt-3 mt-md-0">
+        {% include figure.liquid loading="eager" path="assets/img/Global_SIE_climatology.png" title="example image" class="img-fluid rounded z-depth-1" %}
+    </div>
+</div>
+<div class="caption">
+    Climatology of Global sea ice extent.
+</div>
---
+++ scripts/global_sea_ice.py
@@ -0,0 +1,197 @@
+import numpy as np
+import pooch
+import matplotlib.pyplot as plt
+import matplotlib
+import xarray as xr
+import pandas as pd
+import datetime
+import calendar
+
+
+def plot_global_sea_ice():
+
+    today = datetime.date.today()
+
+    fname_sie_N = pooch.retrieve(
+        url="ftp://sidads.colorado.edu/pub/DATASETS/NOAA/G02135/north/daily/data/N_seaice_extent_daily_v3.0.csv",
+        known_hash=None,
+        path='./'
+    )
+
+    df_SIE_N = pd.read_csv(fname_sie_N, skipinitialspace=True, header=[0],skiprows=[1])
+
+    # convert to proper datetime index
+    df_SIE_N['Date'] = pd.to_datetime(df_SIE_N[['Year','Month','Day']])
+    df_SIE_N.set_index('Date', inplace=True)
+
+    fname_sie_S = pooch.retrieve(
+        url="ftp://sidads.colorado.edu/pub/DATASETS/NOAA/G02135/south/daily/data/S_seaice_extent_daily_v3.0.csv",
+        known_hash=None,
+        path='./'
+    )
+
+    df_SIE_S = pd.read_csv(fname_sie_S, skipinitialspace=True, header=[0],skiprows=[1])
+
+    # convert to proper datetime index
+    df_SIE_S['Date'] = pd.to_datetime(df_SIE_S[['Year','Month','Day']])
+    df_SIE_S.set_index('Date', inplace=True)
+
+    df_SIE = df_SIE_N + df_SIE_S
+
+    # get total number of years in dataset to use later on
+    years = list(df_SIE.index.year.unique())
+
+    # define a colourmap over all the years
+    cmap = matplotlib.cm.YlOrRd(np.linspace(0,1,len(years)))
+
+    # convert to xarray because I'm more comfortable with that
+    ds = xr.Dataset.from_dataframe(df_SIE)
+
+    # calculate daily climatology
+    # smooth over two days to deal with two-daily observations in
+    # beginning of record
+    ds_daily_clim =ds.sel(Date=slice('1981-01-01',
+            '2010-12-31')).groupby('Date.dayofyear').mean(
+            ).rolling(dayofyear=2).mean()
+
+    # calculate daily anomalies
+    ds_anoms = ds.groupby('Date.dayofyear') - ds_daily_clim
+
+    # Calculate standard deviations for anomalies
+    ds_std = ds.sel(Date=slice('1981-01-01','2010-12-31')).groupby(
+    ds.sel(Date=slice('1981-01-01','2010-12-31')).Date.dt.dayofyear).std()
+
+    ds_anoms_std = ds_anoms.groupby(ds_anoms.Date.dt.dayofyear)/ds_std
+
+
+    # plot each year of data on top of each other
+    ##############################################
+
+    plt.figure(figsize=(10,5))
+
+    for i, year in enumerate(range(1978, today.year+1)):
+        ds_year = ds.Extent.sel(Date=slice('{0}-01-01'.format(year),
+                                '{0}-12-31'.format(year))).rolling(Date=7).mean()
+        # ds_year.plot(color=cmap[i])
+        plt.plot(pd.DatetimeIndex(ds_year.Date).dayofyear, ds_year, color=cmap[i])
+
+    plt.plot(pd.DatetimeIndex(ds_year.Date).dayofyear, ds_year, color='k', linewidth=3)
+
+
+    plt.pcolormesh([-20,-15], [0,1], [[1978, 2000],[2000,today.year]], cmap='YlOrRd')
+    CB = plt.colorbar()
+    CB.ax.set_ylabel('Year', fontsize=30)
+
+    plt.text(float(pd.DatetimeIndex(ds_year.Date).dayofyear[-1]), ds_year[-1].data-2, str(today.year), fontsize='large')
+
+    plt.text(15, 14.5, 'Climate-Plots.github.io',
+        fontsize=13, color='black')
+
+    plt.ylabel('Sea Ice Extent\n(millions of square kilometres)')
+    plt.xticks(np.linspace(15,380,13)[:-1], calendar.month_name[1:], rotation=90)
+    plt.xlim(-10,376)
+    plt.ylim(14,29)
+    plt.savefig('../assets/img/Global_sea_ice_by_year.png', bbox_inches='tight', dpi=200)
+
+    # plot daily climatology
+    ########################
+    plt.figure(figsize=(7,4))
+    ds_daily_clim['Extent'].plot()
+    plt.text(150, 19, 'Climate-Plots.github.io',
+        fontsize=12, color='black')
+    plt.savefig('../assets/img/Global_sea_ice_climatology.png', dpi=200, bbox_inches='tight')
+    plt.close()
+
+
+    # plot daily anomalies
+    ######################
+    plt.figure(figsize=(13,5))
+    window=7
+    ds_anoms.Extent.rolling(Date=window).mean().plot(color='k')
+    plt.hlines(0, np.datetime64('1978-09-01'), np.datetime64('2024-05-01'), 'k')
+    plt.scatter(ds_anoms['Date'], ds_anoms['Extent'].rolling(Date=window).mean(),
+                c=ds_anoms['Extent'].rolling(Date=window).mean(), cmap='RdBu', edgecolors=None,
+               vmin= ds_anoms['Extent'].rolling(Date=window).mean().min(),
+               vmax= ds_anoms['Extent'].rolling(Date=window).mean().max())
+
+    plt.text(np.datetime64('1978-09-01'), 3.45,'Global Sea Ice Extent Anomaly', fontsize=20, weight='bold')
+    plt.text(np.datetime64('1978-09-01'), 3,'Relative to 1981-2010 ($10^{6}$ km$^{2}$)', fontsize=15)
+    plt.ylabel('')
+    plt.xlabel('')
+
+    plt.xlim(np.datetime64('1978-05-01'), np.datetime64('2025-01-01'))
+    plt.ylim(-5,2.5)
+    # ax = plt.gca()
+    # ax.spines[['right', 'top']].set_visible(False)
+    # # ax.tick_params(axis='x', which='both', length=0)
+
+    # plt.axis('off')
+    # fig = plt.gcf()
+    # fig.axes.get_yaxis().set_visible(False)
+    plt.text(np.datetime64('1985-01-01'), -4.5, 'Climate-Plots.github.io',
+        fontsize=15, color='black')
+    plt.savefig('../assets/img/Global_sea_ice_anom.png', dpi=300, bbox_inches='tight')
+    plt.close()
+
+
+    # plot each year of anomalies on top of each other
+    ##################################################
+    plt.figure(figsize=(10,5))
+
+    for i, year in enumerate(range(1978, today.year+1)):
+        ds_year = ds_anoms.sel(Date=slice('{0}-01-01'.format(year),
+                                '{0}-12-31'.format(year)))#.rolling(Date=7).mean()
+        plt.plot(ds_year.Date.dayofyear, ds_year.Extent, color='grey')
+
+    plt.plot(ds_year.Date.dayofyear, ds_year.Extent, color='red', linewidth=3)
+
+    plt.text(ds_year.Date.dayofyear[-1].data, ds_year.Extent[-1].data-0.3, str(today.year), fontsize='large')
+
+    plt.ylabel('Sea Ice Extent anomaly\n(millions of square kilometres)')
+    plt.xticks(np.linspace(15,380,13)[:-1], calendar.month_name[1:], rotation=90)
+    plt.hlines(0,-10,376, color='k')
+    plt.xlim(-10,376)
+    plt.text(2, -4.5, 'Climate-Plots.github.io',
+        fontsize=12, color='black')
+    plt.savefig('../assets/img/Global_sea_ice_year_anoms.png', bbox_inches='tight', dpi=200)
+    plt.close()
+
+
+    # Plot anomalies by standard deviation
+    ######################################
+
+    plt.figure(figsize=(10,5))
+
+    for i, year in enumerate(range(1978, today.year+1)):
+        ds_year = ds_anoms_std.sel(Date=slice('{0}-01-01'.format(year),
+                                '{0}-12-31'.format(year)))
+        ds_year_dayofyear = ds_anoms.sel(Date=slice('{0}-01-01'.format(year),
+                                '{0}-12-31'.format(year)))
+        # ds_year.Extent.plot(color=cmap[i])
+        plt.plot(ds_year_dayofyear.Date.dayofyear,
+                 (ds_year.swap_dims({'Date': 'dayofyear'}).Extent).rolling(dayofyear=5).mean(),
+                 color=cmap[i])
+
+    plt.plot(ds_year_dayofyear.Date.dayofyear,
+             (ds_year.swap_dims({'Date': 'dayofyear'}).Extent).rolling(dayofyear=5).mean(),
+             color='k', linewidth=3)
+
+    plt.pcolormesh([-20,-15], [0,1], [[1978, 2000],[2000,today.year]], cmap='YlOrRd')
+    CB = plt.colorbar()
+    CB.ax.set_ylabel('Year', fontsize=30)
+
+    plt.text(ds_year_dayofyear.dayofyear[-1].data+ 10,
+             (ds_year.swap_dims({'Date': 'dayofyear'}).Extent).rolling(dayofyear=5).mean()[-1].data,
+             str(today.year), fontsize='large')
+
+    plt.ylabel('Sea Ice Extent anomaly\n(Standard deviations from the 1981-2010 climatology)')
+    plt.xticks(np.linspace(15,380,13)[:-1], calendar.month_name[1:], rotation=90)
+    plt.hlines(0,-10,376)
+    plt.xlim(-10,376)
+    plt.text(2, -6.5, 'Climate-Plots.github.io',
+    fontsize=12, color='black')
+    plt.savefig('../assets/img/Global_sea_ice_year_anoms_standardised.png', bbox_inches='tight', dpi=200)
+
+if __name__ == '__main__':
+    plot_global_sea_ice()
+
